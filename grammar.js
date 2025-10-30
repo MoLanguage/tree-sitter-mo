f64878c95d87c0acdc57ab2b68bb853c8f9c71b7
earomc <moritz.brunne@gmail.com>
2025-10-30 02:51:27 +0100

    feat: Update grammar

--- grammar.js
+++ grammar.js
@@ -9,9 +9,23 @@
 
 module.exports = grammar({
   name: "mo",
-
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._decl),
+    _decl: ($) => choice($.fn_decl),
+    fn_decl: ($) => seq('fn', $._ident, $._param_list, $._code_block),
+    _code_block: $ => seq('{', repeat($._statement),'}'),
+    _statement: $ => choice(), // TODO: fill in statements
+    var_decl_assign: $ => seq($.variable_ident, $._type, $._decl_assign, $._expr),
+    _decl_assign: $ => /:=/,
+    _expr: $ => choice(), // TODO: fill in expressions
+    _param_list: ($) => seq('(', optional(seq($._param, repeat(seq(',', $._param)))), ')'), // \((param (param,)*)?\)
+    _param: ($) => seq($.variable_ident, $._type),
+    variable_ident: $ => $._ident,
+    _ident: ($) => /[a-zA-Z]+/,
+    _type: $ => choice($.primitive_type, $.primitive_type),
+    primitive_type: $ => choice('bool', 'i32'),
+    array_type: $ => seq('[', optional($.integer_literal), ']', $._type),
+    integer_literal: $ => /[0-9]+/,
+    string_literal: $ => /".*"/
+  },
 });
